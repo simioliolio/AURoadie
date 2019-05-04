5a6b7d75ad4009f7902af5c9614da2a675943c2a
simioliolio <simon@hyperbarn.com>
2019-05-04 13:29:05 +0100

    Initial Commit

---
+++ AURoadie/AURoadie.h
@@ -0,0 +1,19 @@
+//
+//  AURoadie.h
+//  AURoadie
+//
+//  Created by Simon Haycock on 04/05/2019.
+//  Copyright © 2019 Hyper Barn LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AURoadie.
+FOUNDATION_EXPORT double AURoadieVersionNumber;
+
+//! Project version string for AURoadie.
+FOUNDATION_EXPORT const unsigned char AURoadieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AURoadie/PublicHeader.h>
+
+
